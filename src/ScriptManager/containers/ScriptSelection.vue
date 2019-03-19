e1ceea5cc6b469b3bfd7c12256aa69df4a0745b0
Jurgenet <tech.jurgenet@gmail.com>
2019-03-19 15:21:53 +0300

    Added script-selection container

---
+++ src/ScriptManager/containers/ScriptSelection.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+  - var rootClass = 'script-selection'
+
+  div(class=rootClass)
+    div(class=`${rootClass}__heading`)
+      div(class=`${rootClass}__title`) Сценарии
+    div(class=`${rootClass}__body`)
+      ScriptList(:items="scripts" @select="handleSelectScript")
+</template>
+
+<script>
+import ScriptList from '@/components/ScriptList.vue';
+
+export default {
+  name: 'ScriptSelection',
+  components: { ScriptList },
+
+  props: {
+    scripts: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  methods: {
+    handleSelectScript({ scriptId }) {
+      this.$emit('select', { scriptId });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.script-selection {
+  &__heading {
+    border-bottom: 1px solid #e5e5e5;
+  }
+
+  &__title {
+    padding: 0 0 27px 0;
+    font-size: 30px;
+    line-height: 1.24;
+    color: #000000;
+  }
+
+  &__body {
+    margin: 28px 0 0 0;
+  }
+}
+</style>
+
