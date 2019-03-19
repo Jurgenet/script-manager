76ae662ba8ea281389da720c56bca17a6d722965
Jurgenet <tech.jurgenet@gmail.com>
2019-03-19 15:20:27 +0300

    Added button components

---
+++ src/ScriptManager/components/ButtonBack.vue
@@ -0,0 +1,46 @@
+
+<template lang="pug">
+  - var rootClass = 'button-back'
+
+  div(
+    class=rootClass
+    @click="$emit('click')"
+  )
+    img(
+      class=`${rootClass}__icon`
+      src="../../assets/img/left-arrow.svg"
+      alt="left-arrow-img"  
+    )
+    div(class=`${rootClass}__label`) {{ label }}
+</template>
+
+<script>
+export default {
+  name: 'ButtonBack',
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.button-back {
+  cursor: pointer;
+
+  &__icon {
+    display: inline-block;
+  }
+
+  &__label {
+    display: inline-block;
+    padding: 0 0 0 6px;
+
+    text-align: center;
+  }
+}
+</style>
+
---
+++ src/ScriptManager/components/ButtonMain.vue
@@ -0,0 +1,68 @@
+<template lang="pug">
+  - var rootClass = "button-main"
+
+  div(class=rootClass)
+    button(
+      class=`${rootClass}__button`
+      :class="{'button-main__button--alternate': alternate}"
+      @click="$emit('click')"
+    ) {{ label }}
+</template>
+
+<script>
+export default {
+  name: 'ButtonMain',
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    alternate: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.button-main {
+  width: 100%;
+
+  &__button {
+    border: none;
+    padding: 0 0.875rem;
+    height: 2.5rem;
+    width: 100%;
+
+    font-weight: bold;
+    text-align: center;
+    color: #fff;
+    background-color: #00985f;
+
+    outline: none;
+    cursor: pointer;
+  }
+
+  &__button--default {
+    border: solid 1px #dcdcdc;
+
+    color: #333333;
+    background-color: #f2f2f2;
+  }
+
+  &__button--active {
+    color: #ffffff;
+    background-color: #4f0380;
+  }
+
+  &__button--alternate {
+    border: solid 1px #00985f;
+
+    color: #00985f;
+    background-color: #ffffff;
+  }
+}
+</style>
