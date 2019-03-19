457aec7f0f41b308fbbeae71120304842ffe323a
Jurgenet <tech.jurgenet@gmail.com>
2019-03-19 15:21:16 +0300

    Added script-list components

---
+++ src/ScriptManager/components/ScriptList.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  - var rootClass = 'script-list'
+
+  div(class=rooClass)
+    template(v-for="item in items")
+      div(class=`${rootClass}__script` :key="item.id")
+        ScriptListItem(:item="item" @select="handleSelectItem")
+</template>
+
+<script>
+import ScriptListItem from '@/components/ScriptListItem.vue';
+
+export default {
+  name: 'ScriptList',
+  components: { ScriptListItem },
+
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  methods: {
+    handleSelectItem(id) {
+      this.$emit('select', { scriptId: id });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.script-list {
+  &__script {
+    margin: 10px 0 0 0;
+    border-bottom: solid 1px #dcdcdc;
+    padding: 0 0 10px 0;
+  }
+}
+</style>
---
+++ src/ScriptManager/components/ScriptListItem.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+  - var rootClass = 'script-list-item'
+
+  div(class=rootClass)
+    div(class=`${rootClass}__info`)
+      div(class=`${rootClass}__name`) {{ item.name }}
+      div {{ item.description }}
+    div(class=`${rootClass}__controls`)
+      ButtonMain(
+        alternate
+        label="Запустить"
+        @click="$emit('select', item.id)"
+      )
+</template>
+
+<script>
+import ButtonMain from '@/components/ButtonMain.vue';
+
+export default {
+  name: 'ScriptListItem',
+  components: { ButtonMain },
+
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.script-list-item {
+  display: flex;
+
+  &__info {
+    padding: 0 36px 0 0;
+
+    flex: 1;
+  }
+
+  &__name {
+    font-size: 20px;
+    font-weight: bold;
+    color: #00985f;
+  }
+
+  &__description {
+    line-height: 1.54;
+    letter-spacing: normal;
+    color: #444444;
+  }
+}
+</style>
+
+
